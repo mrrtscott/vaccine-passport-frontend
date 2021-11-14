b48adc831873de39ab3e091d4c68d69d1268be9b
Scott <scott.r.ja@gmail.com>
2021-11-13 20:45:56 -0500

    Address section added to the traveller component

--- vaccine-passport/src/app/components/search/search.component.ts
+++ vaccine-passport/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  passportNumberSearch(passportNumber: string){
+    console.log(`passportNumber=${passportNumber}`)
+    this.router.navigateByUrl(`/traveller-information/${passportNumber}`);
   }
 
 }
